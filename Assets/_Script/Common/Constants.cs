bad84281ece3c1073b9fa1fab8ec90c5ef189a37
ZeroDimen <rlaehdrjs951@naver.com>
2025-09-05 17:22:23 +0900

    Add Login Popup
    2025.09.05
    -- UnityWebRequest를 사용하여 서버와 통신
    -- 이름 또는 비밀번호가 DB 저장된 데이터와 같지 않으면우 실패 메시지 출력

--- Assets/_Script/Common/Constants.cs
+++ Assets/_Script/Common/Constants.cs
@@ -1,5 +1,6 @@
 public class Constants
 {
+    public const string ServerURL = "http://localhost:3000";
     public enum GameType { SinglePlay, DualPlay, MultiPlay }
     public enum PlayerType { None, PlayerA, PlayerB }
 
